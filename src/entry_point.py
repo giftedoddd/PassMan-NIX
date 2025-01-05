dafd999ff005f25eee9e68fbf8085202714236bd
giftedodd <giftedoddd@gmail.com>
2025-01-05 06:14:08 +0300

    changing entry_point.py

--- src/entry_point.py
+++ src/entry_point.py
@@ -2,8 +2,8 @@ from modules import image_processor
 from interface.app import App
 
 if __name__ == '__main__':
-    image = image_processor.get_image()
-    app = App(image=image)
+    blur_image = image_processor.get_image()
+    app = App(blur_image=blur_image)
     app.base_init()
     app.make_widgets()
     app.mainloop()
